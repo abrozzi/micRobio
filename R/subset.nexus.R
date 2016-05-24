a56301a1ba2264eac9aa813a37610a3ea26235cf
Alessandro Brozzi <alessandro.brozzi@gmail.com>
2016-05-24 10:25:02 +0200

    new function subset.nexus

--- R/subset.nexus.R
+++ R/subset.nexus.R
@@ -1,34 +1,84 @@
+#' @title Subset a nexus file
+#' @description Reads a nexus file and the tips one wants to keep. It create a nexus file with the selected tips
+#' @param nexus.file
+#' @param tips
+#' @return a nexus file with subset distant matrix
+#' @export
+#'
 subset.nexus <- function(nexus.file, tips, outfile, fromBIGSdb=FALSE){
 
   file = readLines(nexus.file)
   start.MATRIX = match("MATRIX", file)
   ends = which(file == "END;")
-  end.MATRIX = ends[length(ends)]-2
+  end.MATRIX = ends[length(ends)]-1
 
   L = list()
-  tips=c()
+  ids=c()
 
   for(i in (start.MATRIX+1):(end.MATRIX-1)) {
 
-  tip=NA
+  id=NA
 
   line = unlist(strsplit(file[i],"\\\t"))
 
   if(fromBIGSdb){
 
-    tip = unlist(strsplit(line,"\\|"))[2]
-    tips = c(tips, tip)
+    id = unlist(strsplit(line,"\\|"))[2]
+    ids = c(ids, id)
 
   } else {
 
-    tip=line[1]
-    tips = c(tips, tip)
+    id=line[1]
+    ids = c(ids, id)
 
   }
 
-  L[[tip]]=line
-
+ L[[id]]=line[-1]
 
 }
+
+A = matrix(NA,ncol=114,nrow=114)
+
+for(i in 1:length(L)) {
+  columns = length(L[[i]])
+  A[i, 1:columns] = L[[i]]
+}
+
+rownames(A) = ids
+colnames(A) = ids
+
+keep = match(tips, rownames(A))
+
+A_hat = A[keep,keep]
+
+cat("#NEXUS","\n", file=outfile, append=TRUE,sep = "")
+
+cat("BEGIN taxa;","\n", file=outfile, append=TRUE,sep = "")
+dimensions = paste0("DIMENSIONS ntax = ", length(keep),";")
+cat(dimensions,"\n", file=outfile, append=TRUE,sep = "")
+cat("END;","\n", file=outfile, append=TRUE,sep = "")
+
+cat("BEGIN distances;","\n", file=outfile, append=TRUE,sep = "")
+cat(dimensions,"\n", file=outfile, append=TRUE,sep = "")
+cat("FORMAT","\n", file=outfile, append=TRUE,sep = "")
+cat("triangle=LOWER","\n", file=outfile, append=TRUE,sep = "")
+cat("diagonal","\n", file=outfile, append=TRUE,sep = "")
+cat("labels","\n", file=outfile, append=TRUE,sep = "")
+cat("missing=?","\n", file=outfile, append=TRUE,sep = "")
+cat(";","\n", file=outfile, append=TRUE,sep = "")
+cat("MATRIX","\n", file=outfile, append=TRUE,sep = "")
+
+for (i in 1:nrow(A_hat)){
+
+lineToPrint = c(rownames(A_hat)[i], A_hat[i,])
+
+lineToPrint = lineToPrint[!is.na(lineToPrint)]
+lineToPrint = paste(lineToPrint, collapse="\t")
+cat(lineToPrint,"\n", file=outfile, append=TRUE,sep = "")
+
+}
+
+cat(";","\n", file=outfile, append=TRUE,sep = "")
+cat("END;","\n", file=outfile, append=TRUE,sep = "")
 
 } # closes fun
