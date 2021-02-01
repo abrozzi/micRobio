cdb6f5a9b1a568c74b1d17297f5009bacf916e65
ab552791 <alessandro.x.brozzi@gsk.com>
2021-02-01 21:22:21 +0100

    fixed with all possible values

--- R/readSNP.R
+++ R/readSNP.R
@@ -1,34 +1,36 @@
 readSNP <- function(file) {
 
-  OBJ = data.frame(
-    frequency = NA,
-    gene_name = NA,
-    gene_position = NA,
-    gene_product = NA,
-    gene_strand = NA,
-    genes_promoter    = NA,
-    locus_tag  = NA,
-    locus_tags_promoter = NA,
-    mutation_category = NA,
-    position_end = NA,
-    position_start = NA,
-    ref_seq = NA,
-    snp_type      = NA,
-    aa_new_seq = NA,
-    aa_position = NA,
-    aa_ref_seq = NA,
-    codon_new_seq    = NA,
-    codon_number = NA,
-    codon_position = NA,
-    codon_ref_seq = NA,
-    genes_overlapping = NA,
-    locus_tags_overlapping = NA,
-    transl_table = NA
-  )
-
   RL = readLines(file)
 
   totLines = length(RL)
+  M = matrix (NA, ncol= 23, nrow= totLines)
+  colnames(M) = c(
+    "aa_new_seq",
+    "aa_position",
+    "aa_ref_seq",
+    "codon_new_seq",
+    "codon_number",
+    "codon_position",
+    "codon_ref_seq",
+    "frequency",
+    "gene_name",
+    "gene_position",
+    "gene_product",
+    "gene_strand",
+    "genes_promoter",
+    "locus_tag",
+    "locus_tags_promoter",
+    "mutation_category",
+    "position_end",
+    "position_start",
+    "ref_seq",
+    "snp_type",
+    "genes_overlapping",
+    "locus_tags_overlapping",
+    "transl_table"
+  )
+
+  OBJ = as.data.frame(M)
 
   for ( i in 1 : totLines ) {
 
@@ -42,9 +44,12 @@ readSNP <- function(file) {
     keys   = sapply(strsplit(line,"="),function(s) s[1])[start:end]
     values = sapply(strsplit(line,"="),function(s) s[2])[start:end]
 
-    idx = match(keys, colnames(OBJ))
-
-    OBJ[,idx] = values
+    for ( j in 1:ncol(OBJ)) {
+      idx = match(colnames(OBJ)[j], keys)
+      if(!is.na(idx)) {
+        OBJ[i,j] = values[idx]
+      }
+    }
   }
 
   OBJ = OBJ[-1,]
